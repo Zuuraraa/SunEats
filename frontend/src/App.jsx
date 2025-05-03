d538c52badc74c90f5a8df04cc62e3aff916161e
firdanmanggalap <165051977+firdanmanggalap@users.noreply.github.com>
2025-05-03 16:52:44 +0700

    Update App.jsx
    ini aksesoris juga biar bisa diakses pagenya

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { CartProvider } from "./context/CartContext";
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
@@ -7,31 +8,23 @@ import CheckOut from './pages/CheckOut/CheckOut'
 import Menu from'./pages/Menu/Menu'
 import Footer from './components/Footer/Footer'
 import About from './pages/About/About'
-import Contact from './pages/Contact/Contact'
-import Profile from './pages/Profile/Profile'
-import MenuDetail from './pages/MenuDetail/MenuDetail'
-import MyOrder from './pages/MyOrder/MyOrder'
 
 const App = () => {
   return (
-    <>
-      <div className='app'>
+    <CartProvider>
+      <div className='app'> 
         <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/Cart' element={<Cart/>}/>
           <Route path='/Checkout' element={<CheckOut/>}/>
           <Route path='/Menu' element={<Menu/>}/>
-          <Route path='/MyOrder' element={<MyOrder/>}/>
           <Route path='/About' element={<About/>}/>
-          <Route path='/Contact' element={<Contact/>}/>
-          <Route path='/Profile' element={<Profile/>}/>
-          <Route path='/Menu/:MenuId' element={<MenuDetail/>}/>
         </Routes>
       </div>
       <Footer/>
-    </>
+    </CartProvider>
   )
 }
 
-export default App
+export default App;
